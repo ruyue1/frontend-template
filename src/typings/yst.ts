9fb315bdd3939697223b4b9bd18b678ecdede5d6
毛毛包 <2201862726@qq.com>
2026-07-22 10:54:57 +0800

    修复ts报错

--- src/typings/yst.ts
+++ src/typings/yst.ts
@@ -6,5 +6,5 @@ export type YstConfigType = {
   END_URL: string; // 一号通的登出地址
   RESPONSE_TYPE: string; // 一号通认证完成的响应类型
   LOGOUT_SHARE_JS: string; // 同步一事通登出状态的JS文件地址
-  ENV: 'prod' | 'biz' | 'test' // 当前对接的一号通环境
+  ENV: 'prod' | 'biz' | 'test' | 'local'// 当前对接的一号通环境，这个local只是用于解决从github上拉取模板工程时的跨域问题，实际行内是没有这个local的
 }
